8afe656e5e4e3776995238238dbef2f71422ad14
phoenix381 <phoenix0381@gmail.com>
2025-02-02 21:04:33 +0300

    dynamic layout

--- src/app_window.cpp
+++ src/app_window.cpp
@@ -1,6 +1,7 @@
 
 #include <QVBoxLayout>
 #include <QHBoxLayout>
+#include <QSplitter>
 
 #include <QUrl>
 
@@ -22,14 +23,44 @@ AppWindow::AppWindow() {
     sidebar = new QWebEngineView();
     controls = new QWebEngineView();
     
-    // layouts
+    // setting up size
+    controls->setFixedHeight(84);
+    controls->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+
+    // laying out app window
     QVBoxLayout *main_layout = new QVBoxLayout();
-    QHBoxLayout *sidepanel_layout = new QHBoxLayout();
-    QVBoxLayout *page_layout = new QVBoxLayout();
+    QWidget *window = new QWidget();
+
+    window->setLayout(main_layout);
+    this->setCentralWidget(window);
+
+    QSplitter *page_splitter = new QSplitter(Qt::Vertical);
+    QSplitter *sidepanel_spliter = new QSplitter(Qt::Horizontal);
 
     // space between widgets
     main_layout->setContentsMargins(0,0,0,0);
     main_layout->setSpacing(2);
+
+    // styling
+    setStyleSheet(
+        "QWidget { padding: 0px; margin: 0px; }"
+        "QWidget { background-color: #282828; }"
+    );
+
+    page_splitter->setHandleWidth(2);
+    page_splitter->setStyleSheet(
+        "QWidget { background-color: red; }"
+    );
+
+    // laying out widgets
+    page_splitter->addWidget(main_page);
+    page_splitter->addWidget(dev_view);
+
+    sidepanel_spliter->addWidget(page_splitter);
+    sidepanel_spliter->addWidget(sidebar);
+
+    main_layout->addWidget(controls);
+    main_layout->addWidget(sidepanel_spliter);
 
     // init content
     main_page->load(QUrl("https://google.com"));
@@ -37,32 +68,4 @@ AppWindow::AppWindow() {
     controls->load(QUrl("qrc:/html/controls.html"));
     controls->page()->setDevToolsPage(dev_view->page());
 
-    // laying out widgets
-    page_layout->addWidget(main_page);
-    page_layout->addWidget(dev_view);
-
-    sidepanel_layout->addLayout(page_layout);
-    sidepanel_layout->addWidget(sidebar);
-
-    main_layout->addWidget(controls);
-    main_layout->addLayout(sidepanel_layout);
-
-    // setting up size
-    controls->setFixedHeight(84);
-    controls->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
-
-    sidebar->setFixedWidth(650);
-    sidebar->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Preferred);
-
-    // app window
-    QWidget *window = new QWidget();
-
-    setStyleSheet(
-        "QWidget { padding: 0px; margin: 0px; }"
-        "QWidget { background-color: #282828; }"
-
-    );
-
-    window->setLayout(main_layout);
-    this->setCentralWidget(window);
 }
