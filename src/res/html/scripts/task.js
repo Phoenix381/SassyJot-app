080f603122f2b9a79ddcaf606aa470d1dc8150c0
phoenix381 <phoenix381@bk.ru>
2025-06-26 19:51:47 +0300

    proj and task basic layout

---
+++ src/res/html/scripts/task.js
@@ -0,0 +1,47 @@
+
+// ============================================================================
+// web channel init
+// ============================================================================
+
+var projectController;
+
+var task;
+var proj;
+
+// async channel creation
+var channel = new QWebChannel(qt.webChannelTransport, function(channel) {
+    console.log("QWebChannel created for controls");
+    console.log("Available objects:", channel.objects);
+
+    projectController = channel.objects.project_controller;
+
+    // selected task
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+
+    proj = projectController.get_project(proj_id).then(project => {
+        proj = JSON.parse(project);
+        projName.innerHTML = proj.name;
+    });
+
+    task = projectController.get_task(task_id).then(t => {
+        task = JSON.parse(t);
+        taskName.innerHTML = task.name;
+    });
+
+    // setting callbacks here
+
+});
+
+// ============================================================================
+// page elements
+// ============================================================================
+
+
+
+// ============================================================================
+// modal actions
+// ============================================================================
+
+
+
